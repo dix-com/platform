bce4e21e577e98c1f0a261745464eee22af517a5
Sarah Chen <pixelpusher@critter.com>
2022-03-22 19:48:54 +0100

    Optimize user.controller.js queries.

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,25 +1,27 @@
 const express = require("express");
-const userController = require("../controllers/user.controller");
-const paginate = require("../middlewares/paginate");
-const upload = require("../config/multerConfig");
 
+const userController = require("../controllers/user.controller");
+const bookmarkController = require("../controllers/bookmark.controller");
+
+const paginate = require("../middlewares/paginateMiddleware");
+const upload = require("../config/multer");
+
+// Multer fields
+const updateUserFields = [
+    { name: "profileImage", maxCount: 1 },
+    { name: "bannerImage", maxCount: 1 },
+];
+
+// Routes
 const router = express.Router();
 
-// User information
+router.use(isAuthenticated);
+
 router.get("/:username", userController.getUser);
 
-router.get("/:username/crits", paginate, userController.getCrits);
+router.get("/:userId/timeline", paginate, userController.getCrits);
 
-router.get("/:userId/liked_crits", userController.getLikedCrits);
-
-router.put(
-    "/:userId",
-    upload.fields([
-        { name: "profileImage", maxCount: 1 },
-        { name: "bannerImage", maxCount: 1 },
-    ]),
-    userController.updateUser
-);
+router.put("/:userId", upload.fields(updateUserFields), userController.updateUser);
 
 router.put("/:userId/following", userController.followUser);
 
