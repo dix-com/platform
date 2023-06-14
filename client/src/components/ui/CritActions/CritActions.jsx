8805643d208084a268e5906df21f77e996a402a3
Jake Morris <jake.cto@critter.com>
2023-06-14 23:48:34 +0200

    Update crit actions.

--- client/src/components/ui/CritActions/CritActions.jsx
+++ client/src/components/ui/CritActions/CritActions.jsx
@@ -1,135 +1,191 @@
-import "react-circular-progressbar/dist/styles.css";
 import "./styles.css";
 
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-
+import { useState } from "react";
 import { IconContext } from "react-icons";
-import { HiOutlineLocationMarker } from "react-icons/hi";
-import { TbCalendarTime, TbMoodSmile } from "react-icons/tb";
-import { MdOutlinePoll, MdOutlineGifBox } from "react-icons/md";
-import { PiImageSquareBold } from "react-icons/pi";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { PiPencilSimpleLine } from "react-icons/pi";
+import { LuRepeat2 } from "react-icons/lu";
+import { TbMessageCircle2, TbShare2 } from "react-icons/tb";
+import { IoMdStats } from "react-icons/io";
+import { BiBookmark, BiSolidBookmark } from "react-icons/bi";
 
-const CritActions = ({ maxLength, crit, handleCrit, setMedia, setMediaPreview }) => {
-    const handleFileChange = ({ target }) => {
-        const types = ["image/png", "image/jpeg", "image/jpg"];
-        const files = target.files;
-        const image = files?.[0];
+import { LinkButton, FloatOptions } from "../../index"
 
-        if (image && types.includes(image.type)) {
-            const blob = URL.createObjectURL(image);
+import {
+    useLikeCritMutation,
+    useUnlikeCritMutation,
+    useCreateRepostMutation,
+    useDeleteRepostMutation,
+    useCreateBookmarkMutation,
+    useDeleteBookmarkMutation,
+} from "../../../features/api/userApi";
 
-            setMedia(image);
-            setMediaPreview(blob);
-        }
+const CritActions = ({ crit, currentUser, openReplyModal, openQuoteModal }) => {
+    const [recritFloat, setRecritFloat] = useState(false);
+
+    const [createRepost] = useCreateRepostMutation();
+    const [deleteRepost] = useDeleteRepostMutation();
+    const [likeCrit] = useLikeCritMutation();
+    const [unlikeCrit] = useUnlikeCritMutation();
+    const [createBookmark] = useCreateBookmarkMutation();
+    const [deleteBookmark] = useDeleteBookmarkMutation();
+
+    const isBookmarked = currentUser.bookmarks.includes(crit._id);
+    const isReposted = crit.recrits.includes(currentUser._id);
+    const isLiked = crit.likes.includes(currentUser._id);
+
+    const handleRecrit = async (e) => {
+        const recritResult = isReposted
+            ? await deleteRepost({ critId: crit._id })
+            : await createRepost({ critId: crit._id });
+
+        if (recritResult?.error) return;
+
+        if (
+            (isReposted && !recritResult.data?.isReposted) ||
+            (!isReposted && recritResult.data?.isReposted)
+        )
+            closeRecritFloat();
     };
+
+    const handleLike = async (e) => {
+        isLiked
+            ? await unlikeCrit({ id: crit._id })
+            : await likeCrit({ id: crit._id });
+    };
+
+    const handleBookmark = async () => {
+        const bookmarkData = {
+            critId: crit._id,
+            userId: currentUser.id,
+        };
+
+        isBookmarked
+            ? await deleteBookmark(bookmarkData)
+            : await createBookmark(bookmarkData);
+    };
+
+
+    const openRecritFloat = () => setRecritFloat(true);
+    const closeRecritFloat = () => setRecritFloat(false);
 
     return (
         <div className="crit-actions">
-            <div className="attachments">
-                <IconContext.Provider value={{ className: "icon crit-attachments_icon" }}>
-                    <label
-                        type="button"
-                        className="crit-attachments_button blue_round-btn"
-                    >
-                        <input
-                            type="file"
-                            id="image"
-                            accept=".jpg, .jpeg, .png"
-                            style={{
-                                display: "none",
-                            }}
-                            onChange={handleFileChange}
-                        />
-                        <div className="icon-container">
-                            <PiImageSquareBold size="16" />
-                        </div>
-                    </label>
+            <LinkButton
+                className={`action-btn reply blue_round-btn`}
+                onClick={openReplyModal}
+            >
+                <div className="icon-container">
+                    <TbMessageCircle2 className="icon" />
+                </div>
 
-                    <button
-                        type="button"
-                        className="crit-attachments_button blue_round-btn"
-                        disabled
-                    >
-                        <div className="icon-container">
-                            <MdOutlineGifBox size="16" />
-                        </div>
-                    </button>
+                <div className="count-container">
+                    <span>{crit.repliesCount}</span>
+                </div>
+            </LinkButton>
 
-                    <button
-                        type="button"
-                        className="crit-attachments_button blue_round-btn"
-                        disabled
-                    >
-                        <div className="icon-container">
-                            <MdOutlinePoll size="16" />
-                        </div>
-                    </button>
+            <LinkButton
+                className={`action-btn recrit green_round-btn ${isReposted && "applied"}`}
+                onClick={openRecritFloat}
+            >
+                <div className="icon-container">
+                    <LuRepeat2 className="icon" />
+                </div>
 
-                    <button
-                        type="button"
-                        className="crit-attachments_button blue_round-btn"
-                        disabled
-                    >
-                        <div className="icon-container">
-                            <TbMoodSmile size="16" />
-                        </div>
-                    </button>
+                <div className="count-container">
+                    <span>{crit.recrits.length}</span>
+                </div>
 
-                    <button
-                        type="button"
-                        className="crit-attachments_button blue_round-btn"
-                        disabled
+                {recritFloat && (
+                    <IconContext.Provider
+                        value={{ className: "float-icon" }}
                     >
-                        <div className="icon-container">
-                            <TbCalendarTime size="16" />
-                        </div>
-                    </button>
+                        <FloatOptions
+                            isOpen={recritFloat}
+                            onClose={closeRecritFloat}
+                            className="recrit-options"
+                        >
+                            <LinkButton
+                                type="button"
+                                className="float-btn"
+                                onClick={handleRecrit}
+                            >
+                                <div className="float-icon-container">
+                                    <LuRepeat2 />
+                                </div>
 
-                    <button
-                        type="button"
-                        className="crit-attachments_button blue_round-btn"
-                        disabled
-                    >
-                        <div className="icon-container">
-                            <HiOutlineLocationMarker size="16" />
-                        </div>
-                    </button>
-                </IconContext.Provider>
-            </div>
+                                {isReposted ? "Undo Repost" : "Repost"}
+                            </LinkButton>
 
-            <div className="crit-submit">
-                {crit.length > 0 && (
-                    <div
-                        className="progressbar-container"
-                        style={{
-                            width: 25,
-                            height: 25,
-                        }}
-                    >
-                        <CircularProgressbar
-                            value={(crit.length / 280) * 100}
-                            styles={buildStyles({
-                                pathColor: crit.length === maxLength ? "red" : "#1D9BF0",
-                                trailColor: "#2F3336",
-                                backgroundColor: "#3e98c7",
-                            })}
-                        />
-                    </div>
+                            <LinkButton
+                                type="button"
+                                className="float-btn"
+                                onClick={openQuoteModal}
+                            >
+                                <div className="float-icon-container">
+                                    <PiPencilSimpleLine size="21" />
+                                </div>
+                                Quote
+                            </LinkButton>
+                        </FloatOptions>
+                    </IconContext.Provider>
                 )}
+            </LinkButton>
 
-                {crit.length > 0 && <div className="separator"></div>}
+            <LinkButton
+                type="button"
+                className={`action-btn like red_round-btn ${isLiked && "applied"}`}
+                data-type="inner-button"
+                onClick={handleLike}
+            >
+                <div className="icon-container">
+                    {isLiked ? <AiFillHeart className="icon" /> : <AiOutlineHeart className="icon" />}
+                </div>
 
-                <button
+                <div className="count-container">
+                    <span>{crit.likes.length}</span>
+                </div>
+            </LinkButton>
+
+            <LinkButton
+                className="action-btn view blue_round-btn"
+                disabled
+            >
+                <div className="icon-container">
+                    <IoMdStats className="icon" />
+                </div>
+
+                <div className="count-container">
+                    <span>0</span>
+                </div>
+            </LinkButton>
+
+            <div className="crit-actions-container">
+                <LinkButton
                     type="button"
-                    className="blue-btn post-btn"
-                    disabled={crit.length <= 0}
-                    onClick={handleCrit}
+                    className="action-btn bookmark blue_round-btn"
+                    onClick={handleBookmark}
                 >
-                    Crit
-                </button>
+                    <div className="icon-container">
+                        {isBookmarked ? (
+                            <BiSolidBookmark className="icon" />
+                        ) : (
+                            <BiBookmark className="icon" />
+                        )}
+                    </div>
+                </LinkButton>
+
+                <LinkButton
+                    className="action-btn crit-btn blue_round-btn"
+                    disabled
+                >
+                    <div className="icon-container">
+                        <TbShare2 className="icon" />
+                    </div>
+                </LinkButton>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default CritActions;
