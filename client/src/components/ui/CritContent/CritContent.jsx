aac6c6ea09d83064f04110d3708fd216e739664e
Marcus Rodriguez <bugslayer9000@dix.com>
2025-05-31 23:14:55 +0200

    Implement image lazy loading for posts.

--- client/src/components/ui/CritContent/CritContent.jsx
+++ client/src/components/ui/CritContent/CritContent.jsx
@@ -1,8 +1,15 @@
 import "./styles.css";
+// import 'react-lazy-load-image-component/src/effects/blur.css';
 
+import { useState } from "react";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { CritText, LinkButton } from "../../index";
+import { useAppDispatch } from "../../../app/store";
+import { modalActions } from "../../../features/slices/modalSlice";
 
-const CritContent = ({ content = "", media = null, openMediaModal }) => {
+const CritContent = ({ content = "", media = null, measure }) => {
+    const dispatch = useAppDispatch();
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
 
     return (
         <div className="crit-content">
@@ -10,14 +17,27 @@ const CritContent = ({ content = "", media = null, openMediaModal }) => {
 
             {media && (
                 <LinkButton
-                    className="media-container"
-                    onClick={openMediaModal}
+                    className={`media-container ${isImageLoaded && "loaded"}`}
+                    style={{
+                        paddingTop: `calc(${media.height} / ${media.width} * 100%)`
+                    }}
+                    onClick={() => dispatch(modalActions.openModal({
+                        name: "MediaModal",
+                        props: { url: media.url }
+                    }))}
                 >
-
-                    <img
+                    <LazyLoadImage
                         className="crit_media"
-                        src={media?.url}
-                        alt="Crit Media"
+                        style={{
+                            position: 'absolute',
+                            width: '100%',
+                            height: '100%',
+                            top: '0',
+                            left: '0'
+                        }}
+                        src={media.url}
+                        onLoad={measure}
+                        alt="Post Media"
                     />
                 </LinkButton>
             )}
